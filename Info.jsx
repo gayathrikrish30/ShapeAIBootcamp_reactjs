bf9fbef1faeb1b03160d19941434b6559277eca4
Gayathri <52448928+gayathrikrish30@users.noreply.github.com>
2021-06-27 14:50:09 +0530

    Add files via upload

---
+++ Info.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+function Info(){
+  return(
+    <div className="note">
+      <h1>Javascript and React.js</h1>
+      <h4>A basic web dev React js bootcamp</h4>
+      <p>This was an amazing Bootcamp taken up by shapeAI</p>
+    </div>
+    
+      );
+}
+export default Info;
